bfb29d462a7670cc50a3e90d7d498fabe481f633
Dmitriy2Belikov <44783389+Dmitriy2Belikov@users.noreply.github.com>
2020-04-11 22:08:43 +0500

    Added output users in administration

--- USSC.Web/Controllers/AdminController.cs
+++ USSC.Web/Controllers/AdminController.cs
@@ -5,28 +5,33 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using USSC.Services;
 using USSC.Services.PermissionServices;
+using USSC.Services.UserServices.Interfaces;
 
 namespace USSC.Web.Controllers
 {
     public class AdminController : Controller
     {
         private readonly IAccessManager _accessManager;
+        private readonly IUserDataService _userDataService;
         private readonly string _adminSubsystemName;
 
-        public AdminController(IAccessManager accessManager)
+        public AdminController(IAccessManager accessManager, IUserDataService userDataService)
         {
             _accessManager = accessManager;
+            _userDataService = userDataService;
             _adminSubsystemName = Constants.AdminSubsystem;
         }
 
+        [HttpGet]
         [Authorize]
         public async Task<IActionResult> Index()
         {
             var hasPermission = await _accessManager.HasPermission(User.Identity.Name, _adminSubsystemName);
+            var users = _userDataService.GetAllUsers();
 
             if (hasPermission)
             {
-                return View();
+                return View(users);
             }
 
             return Forbid(CookieAuthenticationDefaults.AuthenticationScheme);
